10ef6270f7a539de2d2cbf1e09421a35fa86a4ac
ompatil0305 <ompatil0305@gmail.com>
2026-07-18 23:50:25 -0500

    perf: split vendor chunk and increase chunk size warning limit to resolve build warning

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
